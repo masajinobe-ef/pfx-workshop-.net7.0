8529981cc72a66be4ba65e9de74ed6a5c653820a
masajinobe-ef <priscilla.effects@gmail.com>
2024-03-28 21:47:59 +0500

    v0.2

--- pfx-workshop-.net7.0/App.xaml.cs
+++ pfx-workshop-.net7.0/App.xaml.cs
@@ -4,9 +4,6 @@ using System.Windows;
 
 namespace pfx_workshop_.net7._0
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
     public partial class App : Application
     {
     }
